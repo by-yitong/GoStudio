0fec3acd0909280fe34f5645ce96aecb5d914aa3
by-yitong <55941738+by-yitong@users.noreply.github.com>
2026-09-13 22:02:09 +0800

    chore: release GoStudio 1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jmwl.gostudio"
         minSdk = 26
         targetSdk = 28
-        versionCode = 110
-        versionName = "1.0.10"
+        versionCode = 111
+        versionName = "1.0.11"
 
         ndk {
             abiFilters.add("arm64-v8a")
